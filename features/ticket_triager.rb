560a7b673c850ec9f181208c1892ef49b6f2dbbd
jpaulsingh <jatinder500@yahoo.com>
2012-06-15 10:32:51 -0500

    added class ticket_triager

---
+++ features/ticket_triager.rb
@@ -0,0 +1,25 @@
+class TicketTriager < ActiveRecord::Base                                                                                                 
+   belongs_to :ticket
+   belongs_to :work_unit
+
+   has_many :tickets
+   has_many :work_units
+ 
+   validates_presence_of :ticket_id
+ 
+ 
+  def client
+    project.client
+  end
+  
+  def ticket_long_name
+   "Ticket: [#{id}] - #{project.name} - #{name} ticket for #{client.name}"
+  end
+  
+  def workunit_one_year_old?
+    work_units.last.created_at > 1.year.ago
+ 
+  end
+ 
+end
+
