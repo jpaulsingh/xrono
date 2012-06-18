04b6e9ad20ba0a22d9f46f263b42bd1c920065fe
jpaulsingh <jatinder500@yahoo.com>
2012-06-18 07:32:08 -0500

    modified ticket_tragger.rb

--- app/models/ticket_triager.rb
+++ app/models/ticket_triager.rb
@@ -1,26 +1,30 @@
-class TicketTriager < ActiveRecord::Base
+class TicketTriager < ActiveRecord::Base                                                                                                 
   belongs_to :ticket
   belongs_to :work_unit
-
+  belongs_to :project
+ 
   has_many :tickets
   has_many :work_units
-
+ 
   validates_presence_of :ticket_id
-
+ 
 
   def client
     project.client
   end
 
+  def project
+    ticket.project
+  end
+ 
   def ticket_long_name
-    "Ticket: [#{id}] - #{project.name} - #{name} ticket for #{client.name}"
+   "Ticket: [#{id}] - #{project.name} - #{name} ticket for #{client.name}"
   end
-
+   
   def workunit_one_year_old?
-    work_units.last.created_at > 1.year.ago   
-
+    work_units.last.created_at > 1.year.ago
+   
   end
-
-
-
-end
+ 
+ 
+ end
